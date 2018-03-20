eb3c6c2951c40873cb33c71bfc5bbe0d0bbc475f
Zlatan Radetinac <zlra@zuehlke.com>
2018-03-20 13:30:30 +0100

    Empty client solution

---
+++ fnf.Client/fnf.Client/fnf.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fnf.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
