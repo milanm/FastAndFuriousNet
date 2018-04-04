c8ab7bf503df794ce152d4471812a023177c3bb0
Zlatan Radetinac <zlra@zuehlke.com>
2018-04-04 14:41:40 +0200

    reading arguments from command line

--- fnf.Client/fnf.Client/fnf.Client/Program.cs
+++ fnf.Client/fnf.Client/fnf.Client/Program.cs
@@ -1,23 +1,32 @@
 ﻿using fnf.Client.Client;
 using System;
-using System.Reflection;
-using Ninject;
+using fnf.Client.Pilot;
+using log4net;
+using RabbitMQ.Client;
 
 namespace fnf.Client
 {
     class Program
     { 
-        static void Main()
+        static void Main(string[] args)
         {
-            IKernel kernel = new StandardKernel();
-            kernel.Load(Assembly.GetExecutingAssembly());
-        
-            IPilotApi pilotApi = kernel.Get<IPilotApi>();
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Not all command line arguments provided.\nPlease provide accessCode and TeamId.\nPress any key to continue...");
+                Console.ReadKey();
+                return; 
+            }
 
-            var pilot = new Pilot.Pilot(pilotApi);
+            var serverUrl = args[0]; 
+            var accessCode = args[1];
+            var teamName = args[2];
+
+            IConnectionFactory connectionFactory = new ConnectionFactory() {HostName = serverUrl};
+            IRabbitClient rabbitClient = new RabbitClient(teamName,accessCode,new ChannelRegistry(), new QueueRegistry(), connectionFactory);
+            IPilotApi pilotApi = new PilotApi(rabbitClient, accessCode, teamName);
+            IPilot pilot = new Pilot.Pilot(pilotApi);
             
             pilotApi.ConnectToRabbitMq();
-
             pilot.SubscribeToAllChannels();
 
             Console.ReadKey();
